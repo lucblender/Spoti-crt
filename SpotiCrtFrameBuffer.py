c7a40f9c1d141da9fba3105407d8db317e17d681
lucblender <lucasbonvin@hotmail.com>
2021-04-12 10:51:59 +0200

    added class for crt line effect, and some clean

--- SpotiCrtFrameBuffer.py
+++ SpotiCrtFrameBuffer.py
@@ -10,15 +10,51 @@ from glitch_this import ImageGlitcher
 import random
 from threading import Timer
 
-new_width = 480
+screen_width = 720
+screen_height = 480
 
-glitcher = ImageGlitcher()
-
-blank_picture = Image.new('RGBA', size=(720, 480), color=(0,0,0,255))
+blank_picture = Image.new('RGBA', size=(screen_width, screen_height), color=(0,0,0,255))
 
 image_no_signal = blank_picture.copy()
 image_no_signal.paste(Image.open("no_signal.bmp").convert("RGBA"))
 
+
+class CrtNoiseLine: 
+    
+    noised_line = None    
+    
+    def __init__(self):
+        self.line_width = screen_width
+        self.line_height = random.randrange(1,int(screen_height/15))
+        self.noised_line = self.white_noise_pygame_image(line_width, line_height)
+        self.line_increment = random.randrange(10,20)
+        self.is_living = False
+            
+    def display_line(self):       
+        #TODO may remove the comented line, it's how to do a simple line without noise
+        #img = Image.new('RGBA', size=(line_width, line_height), color=(127,127,127,127))
+        #raw_str  = img.tobytes("raw", 'RGBA')
+        #pygame_image = pygame.image.fromstring(raw_str, img.size, 'RGBA')   
+        #pygame_image = self.white_noise_pygame_image(line_width, line_height) white noise line
+        if self.is_living == True:
+            self.screen.blit(self.noised_line , (0, self.offset_line))        
+            self.offset_line = (self.offset_line + line_increment)%screen_height
+        
+        if self.offset_line == 0:
+            self.is_living = False
+        
+    def white_noise_pygame_image(self, width, height):
+        pil_map = Image.new("RGBA", (width, height), 255)
+        random_grid = map(lambda x: (
+                ((int(random.random() * 256),)*3) + (127,)
+            ), [0] * width * height)
+        pil_map.putdata(list(random_grid))        
+        noise_str = pil_map.tobytes("raw", 'RGBA')
+        noise_image = pygame.image.fromstring(noise_str, (width,height), 'RGBA')
+        return noise_image
+        
+    def revive(self):
+        self.is_living = True
 
 
 class SpotiCrtFrameBuffer :
@@ -28,22 +64,17 @@ class SpotiCrtFrameBuffer :
     track = None
     glitch_images = []
     offset_line = 0
-    noised_line = None
     
     def __init__(self):
-        "Ininitializes a new pygame screen using the framebuffer"
-        # Based on "Python GUI in Linux frame buffer"
-        # http://www.karoltomala.com/blog/?p=679
-        
+
         self.track_picture_updated = False
         self.track_updated = False
+        self.glitcher = ImageGlitcher()
         
         disp_no = os.getenv("DISPLAY")
         if disp_no:
             print("I'm running under X display = {0}".format(disp_no))
-        
-        # Check which frame buffer drivers are available
-        # Start with fbcon since directfb hangs with composite output
+
         drivers =  ['directfb']#directfb, 'fbcon', 'svgalib', 'windib', 'x11', 'dga', 'ggi', 'vgl', 'aalib']
         found = False
         for driver in drivers:
@@ -76,11 +107,17 @@ class SpotiCrtFrameBuffer :
         pygame.display.update()
         
         self.pygame_spotify_image = self.PIL_to_pygame(self.PIL_spotify_image)
+        
+        self.noised_lines = []
+        for i in range(0,5):
+            self.noised_lines.append(CrtNoiseLine())
  
     def __del__(self):
         "Destructor to make sure pygame shuts down, etc."
     
     def get_spotify_image(self, spotify_reader):
+        new_width = screen_height 
+        
         new_track = spotify_reader.get_current_track_info()
         
         if new_track != self.track:    
@@ -120,30 +157,24 @@ class SpotiCrtFrameBuffer :
         self.glitch_images = []
         glitch_factor = random.random()*2+1
         for i in range(0,number):
-            glitch_img = glitcher.glitch_image(self.PIL_spotify_image , glitch_factor, color_offset=True, scan_lines=False).convert("RGBA")#.convert('1')
+            glitch_img = self.glitcher.glitch_image(self.PIL_spotify_image , glitch_factor, color_offset=True, scan_lines=False).convert("RGBA")
             raw_str  = glitch_img.tobytes("raw", 'RGBA')
             self.glitch_images.append(pygame.image.fromstring(raw_str, glitch_img.size, 'RGBA').convert())
 
     def display_glitch_images(self):
         for glitch_image in self.glitch_images:
             self.screen.blit(glitch_image, (0, 0))  
-            self.display_line()
+            self.display_lines()
             pygame.display.flip()
     
-    def display_line(self):
-        line_width = pygame.display.Info().current_w
-        line_height = int(pygame.display.Info().current_h/15)
-        
-        if self.noised_line == None:
-            self.noised_line = self.white_noise_pygame_image(line_width, line_height)
-        
-        img = Image.new('RGBA', size=(line_width, line_height), color=(127,127,127,127))
-        raw_str  = img.tobytes("raw", 'RGBA')
-        pygame_image = pygame.image.fromstring(raw_str, img.size, 'RGBA')   
-        #pygame_image = self.white_noise_pygame_image(line_width, line_height) white noise line
-        self.screen.blit(self.noised_line , (0, self.offset_line))        
-        self.offset_line = (self.offset_line + 10)%480
-        
+    def display_lines(self):
+        for line in self.noised_lines:
+            if line.is_living == False:
+                if random.random()*10 >9:
+                    line.revive()
+            line.display_line()
+            
+    #TODO may be removed since now in CrtNoiseLine class
     def white_noise_pygame_image(self, width, height):
         pil_map = Image.new("RGBA", (width, height), 255)
         random_grid = map(lambda x: (
@@ -164,6 +195,7 @@ spotify_reader = SpotifyReader()
 scope.get_spotify_image_timer(spotify_reader, 5)
 scope.display_spotify_image()
 pygame.display.flip()
+
 while(True):
     scope.compute_glitch_images(random.randrange(10,50))
     scope.display_glitch_images()
@@ -171,7 +203,7 @@ while(True):
     loop_max = random.randrange(2,10)*60
     for i in range(0,loop_max):       
         scope.display_spotify_image()
-        scope.display_line()
+        scope.display_lines()
         pygame.display.update()
         if scope.track_picture_updated == True:
             scope.track_picture_updated  = False  
